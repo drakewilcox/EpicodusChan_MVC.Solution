032e501f5f58c8f5155d63b903b92170ed06f1ea
Drake Wilcox <drakewilcox@gmail.com>
2020-04-01 14:01:17 -0700

    clean up code.
    Co-authored-by: jeremy kale padot <jeremy@padot.us>
    Co-authored-by: Geoff Goetz <geoff.goetz503@gmail.com>

--- EpicodusChan/Models/ApiHelper.cs
+++ EpicodusChan/Models/ApiHelper.cs
@@ -7,7 +7,7 @@ namespace EpicodusChan.Models
   {
     public static async Task<string> GetAll()
     {
-      RestClient client = new RestClient("http://localhost:5000/Messages");
+      RestClient client = new RestClient("http://localhost:5000/api");
       RestRequest request = new RestRequest($"messages", Method.GET);
       var response = await client.ExecuteTaskAsync(request);
       return response.Content;
@@ -15,14 +15,14 @@ namespace EpicodusChan.Models
 
       public static async Task<string> Get(int id)
     {
-      RestClient client = new RestClient("http://localhost:5000/Messages");
+      RestClient client = new RestClient("http://localhost:5000/api/Messages");
       RestRequest request = new RestRequest($"messages/{id}", Method.GET);
       var response = await client.ExecuteTaskAsync(request);
       return response.Content;
     }
        public static async Task Post(string newMessage)
     {
-      RestClient client = new RestClient("http://localhost:5000/Messages");
+      RestClient client = new RestClient("http://localhost:5000/api/Messages");
       RestRequest request = new RestRequest($"messages", Method.POST);
       request.AddHeader("Content-Type", "application/json");
       request.AddJsonBody(newMessage);
@@ -31,7 +31,7 @@ namespace EpicodusChan.Models
 
     public static async Task Put(int id, string newMessage)
     {
-      RestClient client = new RestClient("http://localhost:5000/Messages");
+      RestClient client = new RestClient("http://localhost:5000/api/Messages");
       RestRequest request = new RestRequest($"messages/{id}", Method.PUT);
       request.AddHeader("Content-Type", "application/json");
       request.AddJsonBody(newMessage);
@@ -40,7 +40,7 @@ namespace EpicodusChan.Models
 
     public static async Task Delete(int id)
     {
-      RestClient client = new RestClient("http://localhost:5000/Messages");
+      RestClient client = new RestClient("http://localhost:5000/api/Messages");
       RestRequest request = new RestRequest($"messages/{id}", Method.DELETE);
       request.AddHeader("Content-Type", "application/json");
       var response = await client.ExecuteTaskAsync(request);
@@ -51,7 +51,7 @@ namespace EpicodusChan.Models
 
     public static async Task PutGroup(int id, string newGroup)
     {
-        RestClient client = new RestClient("http://localhost:5000/group");
+        RestClient client = new RestClient("http://localhost:5000/api/Groups");
         RestRequest request = new RestRequest($"groups/{id}", Method.PUT);
         request.AddHeader("Content-Type", "application/json");
         request.AddJsonBody(newGroup);
@@ -59,7 +59,7 @@ namespace EpicodusChan.Models
     }
     public static async Task<string> GetAllGroups()
     {
-      RestClient client = new RestClient("http://localhost:5000/Groups");
+      RestClient client = new RestClient("http://localhost:5000/api/Groups");
       RestRequest request = new RestRequest($"groups", Method.GET);
       var response = await client.ExecuteTaskAsync(request);
       return response.Content;
@@ -67,14 +67,14 @@ namespace EpicodusChan.Models
 
       public static async Task<string> GetGroup(int id)
     {
-      RestClient client = new RestClient("http://localhost:5000/Groups");
+      RestClient client = new RestClient("http://localhost:5000/api/Groups");
       RestRequest request = new RestRequest($"groups/{id}", Method.GET);
       var response = await client.ExecuteTaskAsync(request);
       return response.Content;
     }
        public static async Task PostGroup(string newGroup)
     {
-      RestClient client = new RestClient("http://localhost:5000/Groups");
+      RestClient client = new RestClient("http://localhost:5000/api/Groups");
       RestRequest request = new RestRequest($"groups", Method.POST);
       request.AddHeader("Content-Type", "application/json");
       request.AddJsonBody(newGroup);
@@ -82,7 +82,7 @@ namespace EpicodusChan.Models
     }
     public static async Task DeleteGroup(int id)
     {
-      RestClient client = new RestClient("http://localhost:5000/Groups");
+      RestClient client = new RestClient("http://localhost:5000/api/Groups");
       RestRequest request = new RestRequest($"groups/{id}", Method.DELETE);
       request.AddHeader("Content-Type", "application/json");
       var response = await client.ExecuteTaskAsync(request);
--- EpicodusChan/Startup.cs
+++ EpicodusChan/Startup.cs
@@ -49,7 +49,7 @@ namespace EpicodusChan
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
