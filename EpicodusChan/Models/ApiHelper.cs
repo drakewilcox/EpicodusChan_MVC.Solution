d447b00a600fbd37982227316562be535b577f15
Drake Wilcox <drakewilcox@gmail.com>
2020-04-01 10:48:44 -0700

    add methods for accessing group API.
    Co-authored-by: jeremy kale padot <jeremy@padot.us>
    Co-authored-by: Geoff Goetz <geoff.goetz503@gmail.com>

--- EpicodusChan/Models/ApiHelper.cs
+++ EpicodusChan/Models/ApiHelper.cs
@@ -22,7 +22,7 @@ namespace EpicodusChan.Models
     }
        public static async Task Post(string newMessage)
     {
-      RestClient client = new RestClient("http://localhost:5000/api");
+      RestClient client = new RestClient("http://localhost:5000/Messages");
       RestRequest request = new RestRequest($"messages", Method.POST);
       request.AddHeader("Content-Type", "application/json");
       request.AddJsonBody(newMessage);
@@ -31,7 +31,7 @@ namespace EpicodusChan.Models
 
     public static async Task Put(int id, string newMessage)
     {
-      RestClient client = new RestClient("http://localhost:5000/api");
+      RestClient client = new RestClient("http://localhost:5000/Messages");
       RestRequest request = new RestRequest($"messages/{id}", Method.PUT);
       request.AddHeader("Content-Type", "application/json");
       request.AddJsonBody(newMessage);
@@ -40,8 +40,50 @@ namespace EpicodusChan.Models
 
     public static async Task Delete(int id)
     {
-      RestClient client = new RestClient("http://localhost:5000/api");
+      RestClient client = new RestClient("http://localhost:5000/Messages");
       RestRequest request = new RestRequest($"messages/{id}", Method.DELETE);
+      request.AddHeader("Content-Type", "application/json");
+      var response = await client.ExecuteTaskAsync(request);
+    }
+
+    // Methods for Groups: 
+
+
+    public static async Task PutGroup(int id, string newGroup)
+    {
+        RestClient client = new RestClient("http://localhost:5000/group");
+        RestRequest request = new RestRequest($"groups/{id}", Method.PUT);
+        request.AddHeader("Content-Type", "application/json");
+        request.AddJsonBody(newGroup);
+        var response = await client.ExecuteTaskAsync(request);
+    }
+    public static async Task<string> GetAllGroups()
+    {
+      RestClient client = new RestClient("http://localhost:5000/Groups");
+      RestRequest request = new RestRequest($"groups", Method.GET);
+      var response = await client.ExecuteTaskAsync(request);
+      return response.Content;
+    }
+
+      public static async Task<string> GetGroup(int id)
+    {
+      RestClient client = new RestClient("http://localhost:5000/Groups");
+      RestRequest request = new RestRequest($"groups/{id}", Method.GET);
+      var response = await client.ExecuteTaskAsync(request);
+      return response.Content;
+    }
+       public static async Task PostGroup(string newGroup)
+    {
+      RestClient client = new RestClient("http://localhost:5000/Groups");
+      RestRequest request = new RestRequest($"groups", Method.POST);
+      request.AddHeader("Content-Type", "application/json");
+      request.AddJsonBody(newGroup);
+      var response = await client.ExecuteTaskAsync(request);
+    }
+    public static async Task DeleteGroup(int id)
+    {
+      RestClient client = new RestClient("http://localhost:5000/Groups");
+      RestRequest request = new RestRequest($"groups/{id}", Method.DELETE);
       request.AddHeader("Content-Type", "application/json");
       var response = await client.ExecuteTaskAsync(request);
     }
